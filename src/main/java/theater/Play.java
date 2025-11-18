4509eb4117a8ce8bba9a7d5f3eac1f1cce89ecf5
Tantrayoga <arieltantra1@gmail.com>
2025-11-18 01:48:59 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,24 @@
 package theater;
 
+/**
+ * Represents a theatrical play with a name and a type/genre.
+ * This class is part of the 'theater' package.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
     }
 }
